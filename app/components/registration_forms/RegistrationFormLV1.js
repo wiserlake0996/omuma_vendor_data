a4de47617c932e0597904e84b278ff6c448de046
ibrahim D <wiserlake0996@gmail.com>
2016-11-11 17:37:39 -0500

    added firebase
    added form submit to firebase

--- app/components/registration_forms/RegistrationFormLV1.js
+++ app/components/registration_forms/RegistrationFormLV1.js
@@ -16,8 +16,43 @@ class RegistrationFormLV1 extends React.Component{
 
     handleFormSubmit(e){
         e.preventDefault();
-        this.pageRedirect();
-        //alert("Form submittion acknowledged!");
+        //this.pageRedirect();
+        this.collectFormData();
+    }
+
+    collectFormData(){
+        var newVendorKey = firebase.database().ref('registration_level_1/vendors').push().key;
+        var updates = {};
+
+        if(this.refs.business_name.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/name"] = this.refs.business_name.value;
+        }if(this.refs.business_email.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/email"] = this.refs.business_email.value;
+        }if(this.refs.business_address.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/address"] = this.refs.business_address.value;
+        }if(this.refs.business_contact_number.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/phone_number"] = this.refs.business_contact_number.value;
+        }if(this.refs.facebook.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/facebook"] = this.refs.facebook.value; 
+        }if(this.refs.instagram.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/instagram"] = this.refs.instagram.value;
+        }if(this.refs.snapchat.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/snapchat"] = this.refs.snapchat.value;
+        }if(this.refs.twitter.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/twitter"] = this.refs.twitter.value;
+        }if(this.refs.owners_name.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/owners_name"] = this.refs.owners_name.value;
+        }if(this.refs.owners_email.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/owners_email"] = this.refs.owners_email.value ;
+        }if(this.refs.owners_number.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/owners_number"] = this.refs.owners_number.value;
+        }if(this.refs.vendor_type.value != ""){
+            updates['registration_level_1/vendors/' + newVendorKey + "/vendor_type"] = this.refs.vendor_type.value;
+        }
+
+        firebase.database().ref().update(updates).then(function(){
+            this.pageRedirect();
+        }.bind(this));
     }
 
     pageRedirect(){
@@ -32,7 +67,7 @@ class RegistrationFormLV1 extends React.Component{
 
     render(){
         return (
-        <div>
+            <div>
                 <div className="row">
                     <h3 className="col m8 offset-m3"> Registration Form </h3>
                 </div>
@@ -43,9 +78,11 @@ class RegistrationFormLV1 extends React.Component{
 
                 <div className="row">
                     <form className="s12" onSubmit={this.handleFormSubmit}>
+
+
                     <div className="row">
                         <div className="input-field col s12">
-                            <input ref ="business_name" id="business_name" type="text" className="validate"/>
+                            <input required ref ="business_name" id="business_name" type="text" className="validate"/>
                         <label for="business_name">Name</label>
                         </div>
                         <div className="input-field col s12">
@@ -66,7 +103,22 @@ class RegistrationFormLV1 extends React.Component{
                             <input ref="business_contact_number" id="business_contact_number" type="text" className="validate"/>
                             <label for="business_contact_number">Contact number</label>
                         </div>
-                    </div>          
+                    </div>   
+                    <div className="row">
+
+                        <div className="input-field col s12">
+                            <select required ref="vendor_type">
+                            <option value="" disabled selected>Choose your option (required) </option>
+                            <option value="product">Product Vendor</option>
+                            <option value="service">Service Vendor</option>
+                            <option value="location">Location Vendor</option>
+                            <option value="other">Other</option>
+                            </select>
+                            <label>Vendor Type</label>
+                        </div>       
+
+                    </div>
+
 
                     <div className="row">
                         <div className="input-field col s12 m3">
@@ -97,8 +149,8 @@ class RegistrationFormLV1 extends React.Component{
 
                    <div className="row">
                         <div className="input-field col s12">
-                            <input ref="business_owners_name" id="business_owners_name" type="text" className="validate"/>
-                            <label for="business_owners_name">owners name</label>
+                            <input ref="owners_name" id="owners_name" type="text" className="validate"/>
+                            <label for="owners_name">owners name</label>
                         </div>
                     </div>
  
@@ -107,28 +159,28 @@ class RegistrationFormLV1 extends React.Component{
                         <div className="input-field col s12 m6">
                             <i className="material-icons prefix">phone</i>
 
-                            <input ref="business_owners_number" placeholder="optional" id="business_owners_number" type="text" className="validate"/>
-                            <label for="business_owners_number">phone number</label>
+                            <input ref="owners_number" placeholder="optional" id="owners_number" type="text" className="validate"/>
+                            <label for="owners_number">phone number</label>
                         </div>
 
                         <div className="input-field col s12 m6">
                             <i className="material-icons prefix">email</i>
 
-                            <input ref="business_owners_email" placeholder="for app launch updates" id="business_owners_number" type="text" className="validate"/>
-                            <label for="business_owners_email"> email</label>
+                            <input required ref="owners_email" placeholder="for app launch updates" id="owners_email" type="text" className="validate"/>
+                            <label for="owners_email"> email</label>
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col m4 s4 offset-m5">
-                              <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-                                    <i className="material-icons right">send</i>
-                                </button>
+                            <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                                <i className="material-icons right">send</i>
+                            </button>
                         </div>
                     </div>
                     </form>
                 </div>
-        </div>
+            </div>
         );
     }
 }
